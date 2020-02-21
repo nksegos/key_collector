6fe829c0cfe122174fcbef09f86d6a4e3e76f32a
nksegos <nksegos@protonmail.com>
2020-02-21 01:30:24 +0100

    added handling for false positives

--- skel_script.sh
+++ skel_script.sh
@@ -35,8 +35,12 @@ else
 		
 		cat $HASH_LIST | while IFS= read -r line; do 		
 			DIFF_FILE=$(mktemp)
+			KEY_FILE=${KEY_DIR}/private_key_$(echo $GIT_URL | awk -F/ '{print $NF}' | awk -F. '{print $1}')_${line}
 			git show $line | sed -r "s/^([^-+ ]*)[-+ ]/\\1/" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g" > $DIFF_FILE; 
-			awk '/^-----BEGIN [A-Z]* PRIVATE (KEY|KEY BLOCK)-----$/{flag=1}/^-----END [A-Z]* PRIVATE (KEY|KEY BLOCK)-----$/{print;flag=0}flag' $DIFF_FILE > ${KEY_DIR}/private_key_$(echo $GIT_URL | awk -F/ '{print $NF}' | awk -F. '{print $1}')_${line}
+			awk '/^-----BEGIN [A-Z]* PRIVATE (KEY|KEY BLOCK)-----$/{flag=1}/^-----END [A-Z]* PRIVATE (KEY|KEY BLOCK)-----$/{print;flag=0}flag' $DIFF_FILE > $KEY_FILE
+			if [[ "$(wc -l $KEY_FILE | awk -F" " '{print $1}')" == "0" ]]; then
+				rm -f $KEY_FILE
+			fi
 			rm -f $DIFF_FILE
 		done
 		cd /tmp/
@@ -44,7 +48,12 @@ else
 			rm -rf $GIT_DIR
 		fi
 		
-		echo "Collected keys at: $KEY_DIR"
+		if [[ "$(ls -1 $KEY_DIR | wc -l)" == "0" ]]; then
+			rmdir $KEY_DIR
+			echo "No keys found."
+		else
+			echo "Collected keys at: $KEY_DIR"
+		fi
 	fi
 fi
 
